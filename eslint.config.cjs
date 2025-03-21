28610e74509139c8906c912486d77312ae54975d
Christopher Jeffrey <chjjeffrey@gmail.com>
2025-03-21 12:16:44 +0900

    lib/yul.js: new struct handling.

---
+++ eslint.config.cjs
@@ -0,0 +1,39 @@
+'use strict';
+
+const rc = require('bslintrc');
+
+module.exports = [
+  rc.configs.recommended,
+  rc.configs.bcoin,
+  {
+    languageOptions: {
+      globals: {
+        ...rc.globals.node
+      },
+      ecmaVersion: 'latest'
+    }
+  },
+  {
+    files: [
+      'bin/yulc',
+      '**/*.js',
+      '*.js'
+    ],
+    languageOptions: {
+      sourceType: 'commonjs'
+    }
+  },
+  {
+    files: ['test/{,**/}*.{js,cjs,mjs}'],
+    languageOptions: {
+      globals: {
+        ...rc.globals.mocha,
+        register: 'readable'
+      }
+    },
+    rules: {
+      'max-len': 'off',
+      'prefer-arrow-callback': 'off'
+    }
+  }
+];
